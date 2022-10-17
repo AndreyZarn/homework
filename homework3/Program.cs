035dfd22a55548f57098f7688375593011db4655
AndreyZ <mr.zarnitsyn48@gmail.com>
2022-10-17 23:57:19 +0300

    Задача 29

---
+++ homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] arr = new int[8];
+//arr[8] = new Random().Next(1,100);
+void Ind (int[] arr)
+{
+    for (int i = 0; i<arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        Console.Write(",");
+    }
+
+}
+void Rand (int[] arr)
+{
+   for (int i = 0; i<arr.Length; i++)
+   {
+   arr[i] = new Random().Next(1,100);
+   }
+}
+Rand(arr);
+Ind(arr);
+Console.WriteLine();
