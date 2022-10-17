30f7f4841b28cf8d6dc06d2913d20ff9fee8d936
AndreyZ <mr.zarnitsyn48@gmail.com>
2022-10-17 23:55:36 +0300

    Задача 25

---
+++ homework1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int sum = 1;
+void Sum(int a, int b)
+ {
+    
+    for (int i = 1; i<=b; i++)
+    {
+        sum = (sum*a);
+    }
+
+  Console.WriteLine(sum);
+ }
+System.Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Sum(a,b);
